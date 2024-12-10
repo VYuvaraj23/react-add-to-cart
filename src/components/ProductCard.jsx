64b652d4095e632233eb16b019d4ecc14f1f5e2c
Yuvaraj V <yuvaraj8752@gmail.com>
2024-12-10 10:31:39 +0530

    loading update

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,7 @@
 export default function ProductCard(props) {
   const { product,addToCart } = props;
   return (
-    <div className="border p-4 rounded-lg shadow-lg flex flex-col justify-between gap-2 hover:ring hover:scale-[1.02] opacity-100">
+    <div className="border p-4 rounded-lg shadow-lg flex flex-col justify-between gap-2 hover:ring opacity-100">
       <section className="font-bold lg:text-lg text-md flex flex-col gap-2">
       <img src={product.image} alt={product.title} className="w-full h-48 object-contain" />
       <span className="">{product.title}</span>
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -2,19 +2,33 @@ import { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
 
 export default function ProductList(props) {
+  const [loading, setLoading] = useState(true);
   const [Products, setProducts] = useState([]);
   const { addToCart } = props;
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
-      .then((data) => setProducts(data))
-      .catch((error) => console.log("Fetch Api Products : ", error));
+      .then((data) => {
+        setProducts(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log("Fetch Api Products : ", error);
+        setLoading(false);
+      });
   }, []);
   return (
-    <div className="mt-[84px] grid gap-10 lg:grid-cols-4 md:grid-cols-2 grid-cols-1 px-10 py-5  " >
-      {Products.map(product => (
-        <ProductCard key={product.id} product={product} addToCart={addToCart} />
-      )        
+    <div className="my-[84px] grid gap-10 lg:grid-cols-4 md:grid-cols-2 grid-cols-1 px-10 py-5  ">
+      {loading ? (
+        <p className="col-span-4 text-3xl font-semibold text-center">Loading... </p>
+      ) : (
+        Products.map((product) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+            addToCart={addToCart}
+          />
+        ))
       )}
     </div>
   );
