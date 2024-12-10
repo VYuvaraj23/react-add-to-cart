72107cae8d26ff1c1b4030a74a0e9a5e8d727d39
Yuvaraj V <yuvaraj8752@gmail.com>
2024-12-10 10:52:19 +0530

    font-size modify

--- src/components/cartModel.jsx
+++ src/components/cartModel.jsx
@@ -12,7 +12,7 @@ export default function CartModel(props) {
 
       </button>
 
-      <div className="bg-white text-black font-bold text-lg m-auto w-full rounded-xl p-10 space-y-10 max-h-96 overflow-auto ">
+      <div className="bg-white text-black font-regular lg:font-bold lg:text-lg text-xs m-auto w-full rounded-xl p-10 space-y-10 max-h-96 overflow-auto ">
       <h1 className="text-3xl font-bold  text-center">Cart</h1>
 
         {cart.length > 0 ? (
@@ -26,9 +26,9 @@ export default function CartModel(props) {
                   alt={item.title}
                   className="w-16 h-16 object-contain"
                 />
-                <span>{item.title}</span>
+                <span className="font-regular">{item.title}</span>
                 <button onClick={() => removeCart(item.id)}
-                className="bg-gray-800 text-white px-6 py-2 rounded-lg"
+                className="bg-gray-800 text-white px-3 py-1 md:px-6 md:py-2 rounded-lg"
                 >Remove</button>
               </div>
             );
